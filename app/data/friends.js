0e1cf022c1996ab6538b666da678035cd8f0dc69
Matt Bajorek <bajorek.matthew@gmail.com>
2016-07-26 12:16:40 -0400

    Finished app except for making custom questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,32 +3,64 @@
     "name": "Ahmed",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores": [
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
     ]
   },
   {
-    "name": "asdfa",
-    "photo": "asdf",
+    "name": "Jeremiah Scanlon",
+    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
     "scores": [
-      "3",
-      "3",
-      "3",
+      "5",
+      "2",
       "2",
       "2",
       "4",
       "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    "name": "Jordan Biason",
+    "photo": "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+    "scores": [
+      "4",
       "4",
       "2",
-      "3"
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  },
+  {
+    "name": "Matt Bajorek",
+    "photo": "http://matt-bajorek-portfolio.herokuapp.com/img/profilepic1.jpg",
+    "scores": [
+      "5",
+      "3",
+      "4",
+      "2",
+      "3",
+      "4",
+      "1",
+      "4",
+      "2",
+      "4"
     ]
   }
 ]
